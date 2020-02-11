b7abab6538503caf36ca6767ed7949bc38a6a99b
Călin Ardelean <mmn80cpu@gmail.com>
2020-02-11 23:20:18 +0200

    Added highlighters to objects

---
+++ Assets/Highlightable.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Highlightable : MonoBehaviour
+{
+    public Material highlightMaterial;
+
+    private Renderer ownRenderer = null;
+    private Material originalMaterial;
+
+    private void Start()
+    {
+        ownRenderer = GetComponent<Renderer>();
+        originalMaterial = ownRenderer?.material;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!ownRenderer)
+            return;
+        ownRenderer.material = highlightMaterial;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!ownRenderer)
+            return;
+        ownRenderer.material = originalMaterial;        
+    }
+}
