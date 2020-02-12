f5d62b93bd2aff69fc1c8518029129df37ca7967
Călin Ardelean <mmn80cpu@gmail.com>
2020-02-12 15:55:11 +0200

    Move camera instead of changing the object

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float AngularSpeed, ZoomSpeed, AngularAcceleration, ZoomAcceleration;
+
+    float aSpeed, zSpeed;
+    Camera cam;
+
+    void Start() => cam = GetComponent<Camera>();
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            aSpeed = -AngularSpeed;
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            aSpeed = AngularSpeed;
+        else if (aSpeed < 0)
+            aSpeed = Mathf.Min(aSpeed + AngularAcceleration * Time.deltaTime, 0);
+        else if (aSpeed > 0)
+            aSpeed = Mathf.Max(aSpeed - AngularAcceleration * Time.deltaTime, 0);
+        if (aSpeed != 0 && cam)
+            cam.transform.RotateAround(Vector3.zero, Vector3.up, aSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            zSpeed = ZoomSpeed;
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            zSpeed = -ZoomSpeed;
+        else if (zSpeed < 0)
+            zSpeed = Mathf.Min(zSpeed + ZoomAcceleration * Time.deltaTime, 0);
+        else if (zSpeed > 0)
+            zSpeed = Mathf.Max(zSpeed - ZoomAcceleration * Time.deltaTime, 0);
+        if (zSpeed != 0 && cam)
+        {
+            var p = cam.transform.position;
+            if (p.sqrMagnitude > 20 || zSpeed < 0)
+            {
+                p = Vector3.MoveTowards(p, Vector3.zero, zSpeed * Time.deltaTime);
+                cam.transform.position = p;
+            }
+        }
+    }
+}
--- Assets/Control.cs
+++ Assets/Control.cs
@@ -4,13 +4,6 @@ public class Control : MonoBehaviour
 {
     void Update()
     {
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical") + 1;
-
-        var t = gameObject.transform;
-        t.localRotation = Quaternion.Euler(0, h * 180, 0);
-        t.localScale = v * Vector3.one;
-
         if (Input.GetKeyDown(KeyCode.Q) && Input.GetKey(KeyCode.LeftControl))
             Application.Quit();
     }
