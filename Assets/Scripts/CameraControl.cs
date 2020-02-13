285f2483f039762b4f62edc69cd043098573227a
Călin Ardelean <mmn80cpu@gmail.com>
2020-02-13 23:47:16 +0200

    Reimplement zoom for ortho camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,6 +7,8 @@ public class CameraControl : MonoBehaviour
     float aSpeed, zSpeed;
     Camera cam;
 
+    public static bool Orthographic = false;
+
     void Start() => cam = GetComponent<Camera>();
 
     void Update()
@@ -32,17 +34,32 @@ public class CameraControl : MonoBehaviour
             zSpeed = Mathf.Max(zSpeed - ZoomAcceleration * Time.deltaTime, 0);
         if (zSpeed != 0 && cam)
         {
-            var p = cam.transform.position;
-            if (p.sqrMagnitude > 20 || zSpeed < 0)
+            if (Orthographic)
             {
-                p = Vector3.MoveTowards(p, Vector3.zero, zSpeed * Time.deltaTime);
-                cam.transform.position = p;
+                var scale = cam.orthographicSize;
+                if (scale > 1 || zSpeed < 0)
+                {
+                    scale -= zSpeed * Time.deltaTime * 0.5f;
+                    cam.orthographicSize = scale;
+                }
+            }
+            else
+            {
+                var p = cam.transform.position;
+                if (p.sqrMagnitude > 20 || zSpeed < 0)
+                {
+                    p = Vector3.MoveTowards(p, Vector3.zero, zSpeed * Time.deltaTime);
+                    cam.transform.position = p;
+                }
             }
         }
 
         if (Input.GetKeyUp(KeyCode.Alpha1))
         {
-            cam.orthographic = !cam.orthographic;
+            Orthographic = !Orthographic;
+            if (Orthographic)
+                cam.orthographicSize = 4;
+            cam.orthographic = Orthographic;
         }
     }
 }
